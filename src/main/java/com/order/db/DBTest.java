feae309460b9268108d739f610972f16f74cf6fe
HQ <qiang.huang1991@gmail.com>
2015-06-02 22:59:12 +0800

    Oracle JDBC 测试

---
+++ src/main/java/com/order/db/DBTest.java
@@ -0,0 +1,14 @@
+package com.order.db;
+
+public class DBTest {
+
+	/**
+	 * 如何在bolt中使用Oracle的JDBC.
+	 */
+	public static void main(String[] args) {
+		DB db = new DB();
+		db.insertUser("", "", "");
+		db.insertUser("", "", "");
+	}
+
+}
