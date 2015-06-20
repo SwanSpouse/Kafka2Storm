62c87a8ecfec5fdba90512b9501c0fd6918478c5
limingji <swanspouse@126.com>
2015-06-20 10:31:51 +0800

    初始化分支

--- src/main/java/com/order/databean/TimeCacheStructures/RealTimeCacheList.java
+++ src/main/java/com/order/databean/TimeCacheStructures/RealTimeCacheList.java
@@ -37,7 +37,7 @@ public class RealTimeCacheList<T> implements Serializable{
     protected static Object LOCK = null;
     protected transient Thread cleaner = null;
     protected TimeOutCallback timeOutCallback = null;
-    protected int expirationSecs = 0;
+    protected int expiratonSecs = 0;
 
     public RealTimeCacheList(int expiratonSecs) {
         this(expiratonSecs, null);
@@ -49,15 +49,9 @@ public class RealTimeCacheList<T> implements Serializable{
         currentList = new LinkedHashMap<T, LinkedList<Long>>();
 
         this.timeOutCallback = timeOutCallback;
-        this.expirationSecs = expirationSecs;
-
-    }
-
-    public void startCleaner() {
-        if (cleaner != null) {
-            return ;
-        }
+        this.expiratonSecs = expirationSecs;
         final long sleepTime = expirationSecs * 1000L;
+
         cleaner = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -88,8 +82,7 @@ public class RealTimeCacheList<T> implements Serializable{
      * 在获取size之前对oldlist 和 currentlist中的过期数据进行清除
      * */
     public int size(long currentTime) {
-        startCleaner();
-        long timeThreashold = currentTime - expirationSecs * 1000L;
+        long timeThreashold = currentTime - expiratonSecs * 1000L;
         synchronized (LOCK) {
             this.removeTimeOutData(oldList, timeThreashold);
             this.removeTimeOutData(currentList, timeThreashold);
@@ -160,7 +153,6 @@ public class RealTimeCacheList<T> implements Serializable{
     }
 
     public void put(T value, Long date) {
-        startCleaner();
         synchronized (LOCK) {
             long currentTime;
             if (date != null) {
@@ -261,7 +253,10 @@ public class RealTimeCacheList<T> implements Serializable{
         if (oldList.size() == 0 && currentList.size() == 0) {
             return " EMPTY ";
         }
-        return oldList.toString() + "==" + currentList.toString();
+        String str = "";
+        for (T key : oldList.keySet()) {
+            str += " { " + key + " : " + oldList.get(key) + " " + currentList.get(key) + " } ";
+        }
+        return str;
     }
-
 }
